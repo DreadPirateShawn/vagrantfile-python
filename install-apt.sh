896a823691fb1b252baed0edc87c3a0185091fd9
DreadPirateShawn <dreadpirateshawn@gmail.com>
2014-08-02 00:48:53 -0700

    Packages: Added python-pip.

--- install-apt.sh
+++ install-apt.sh
@@ -5,5 +5,5 @@ echo "# apt-get"
 
 apt-get update
 apt-get install -y git-core
-apt-get install -y ipython python-pyke python-texttable
+apt-get install -y ipython python-pip python-pyke python-texttable
 
